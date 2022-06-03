a956683df4a4872472ae7bf489fe36991a6e22c9
mainazarov1 <mainazarov1@gmail.com>
2022-06-03 22:50:10 +0600

    feat: add forms components - T3-61-admin-auth

---
+++ src/components/Forms/FormRecoveryWithLogin.jsx
@@ -0,0 +1,54 @@
+import { VisibilityOutlined } from "@mui/icons-material";
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import { ButtonApp } from "../ButtonApp/ButtonApp";
+import { InputAppContainer } from "../InputApp/InputAppContainer";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+export const FormRecoveryWithLogin = ({prevStep, nextStep}) => {
+  return (
+		<Box
+			position='relative'
+			maxWidth="400px"
+			height='450px'
+      width="calc(100% - 50px)"
+      m="auto"
+      p="25px"
+			component="form"
+			onSubmit={nextStep}
+    >
+			<Stack
+				position='absolute'
+				top='-100px'
+        mt={"24px"}
+        children={
+          <ButtonApp
+						variant="text"
+						colorText='black'
+            icon={<ArrowBackIosNewIcon />}
+						title="Назад"
+						hover={false}
+						prevStep={prevStep}
+          />
+        }
+      />
+      <Typography variant="h3" pb="40px">
+        Восстоновление пароля
+      </Typography>
+      <Stack direction="column" spacing="20px" pb="60px">
+        <InputAppContainer
+          label="Логин"
+          type="email"
+          // {...register("email")}
+        />
+      </Stack>
+      <ButtonApp
+        // className='auth__button'
+        variant="contained"
+        title="Продолжить"
+        type="submit"
+				disabled={false}
+				// fullWidth={true}
+      />
+    </Box>
+  );
+};
