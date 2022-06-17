cb04d00e6c5a11d791a0de8066c88bfae2875b31
mainazarov1 <mainazarov1@gmail.com>
2022-06-17 14:56:23 +0600

    T3-232 #comment feat: add icons #in-progress

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,10 +3,12 @@ import medTechViolet from './logo_violet.svg'
 import calendar from './calendar.svg'
 import checkList from './checkList.svg'
 import medTechWhite from './logo_med_tech-white.svg'
+import arrow from './arrow.svg'
 export const icons = {
 	medTech,
 	medTechWhite,
 	medTechViolet,
 	calendar,
-	checkList
+	checkList,
+	arrow
 }
---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+import manualImg from './manual-bg.jpeg'
+import manualImg2 from './manual-bg2.jpeg'
+import manualImg3 from './manual-bg3.jpeg'
+import manualImg4 from './manual-bg4.jpeg'
+
+export const images = {
+	manualImg,
+	manualImg2,
+	manualImg3,
+	manualImg4,
+}
