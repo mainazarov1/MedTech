c3944fcdd4ce0ca1d2876d564176b76f4f171c6d
mainazarov1 <mainazarov1@gmail.com>
2022-06-26 20:08:53 +0600

    T3-232 - feat: add icons and styles for components and functionality

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,35 +1,62 @@
-import { Label } from "@mui/icons-material";
-import {
-  Box,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import React from "react";
+import { CalendarCustom } from "../Calendar/CalendarCustom";
 import { SelectBtn } from "../Select/Select";
 import { TableCustom } from "../TableCustom/TableCustom";
-import style from './Schedule.module.css'
+import style from "./Schedule.module.css";
+
 export const Schedule = () => {
-	const names = [
-		'Махмудходжаева. Д',
-		'Паненко. Н',
-		'Фралова А.Г',
-		'Паненко. Н',
-		'Пешнограй К. Р',
-	]
+  const names = [
+    "Махмудходжаева. Д",
+    "Паненко. Н",
+    "Фралова А.Г",
+    "Паненко. Н",
+    "Пешнограй К. Р",
+	];
+	
   return (
     <section className={style.schedule}>
-      <Typography
-        component={"h3"}
-        children={"Список запланированных встреч"}
-        sx={{
-          fontSize: "24px",
-        }}
-      />
-      <Stack direction={'row'} gap={'20px'} marginBottom={'35px'} >
-				<SelectBtn label={'Врач'} names={names} />
-				<SelectBtn label={'Пациент'} names={names} />
+      <Stack direction={{xs: 'column', lg: 'row'}} gap="20px">
+				<Stack
+					flexBasis={'100%'}
+					flexShrink={1}
+          sx={{
+						background: "#FFFFFF",
+						padding: '30px 25px',
+						borderRadius: '10px',
+						border: '1px solid #F1F0F3',
+          }}
+        >
+          <Typography
+            component={"h3"}
+            children={"Список запланированных встреч"}
+            sx={{
+              fontSize: "24px",
+            }}
+          />
+          <Stack direction="row" gap={"20px"} marginBottom={"35px"}>
+            <SelectBtn label={"Врач"} names={names} />
+            <SelectBtn label={"Пациент"} names={names} />
+          </Stack>
+          <TableCustom />
+        </Stack>
+				<Stack
+					flexBasis={'400px'}
+					flexGrow={'0'}
+					flexShrink={'0'}
+					direction={{xs: 'row', lg: 'column'}}
+          sx={{
+						background: "#FFFFFF",
+						padding: '22px 20px',
+						borderRadius: '10px',
+						border: '1px solid #F1F0F3',
+						gap: '50px 0',
+          }}
+        >
+					<CalendarCustom />
+					{/* <CalendarCustom /> */}
+        </Stack>
       </Stack>
-      <TableCustom />
     </section>
   );
 };
--- src/components/TableCustom/TableCustom.jsx
+++ src/components/TableCustom/TableCustom.jsx
@@ -1,114 +1,160 @@
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
 } from "@mui/material";
 import React from "react";
-
+import { icons } from "../../assets/icons";
+import styles from './TableCustom.module.css'
 export const TableCustom = () => {
-  const columns = [
-    { id: "number", label: "№", maxWidth: 25 },
-    { id: "doctor", label: "ФИО врача", minWidth: 150 },
-    {
-      id: "patient",
-      label: "ФИО пациента",
-      minWidth: 150,
-      format: (value) => value.toLocaleString("en-US"),
-    },
-    {
-      id: "date",
-      label: "Дата",
-      minWidth: 100,
-      format: (value) => value.toLocaleString("en-US"),
-    },
-    {
-      id: "time",
-      label: "Время",
-      minWidth: 100,
-      format: (value) => value.toFixed(2),
-    },
-  ];
-  function createData(number, doctor, patient, date, time) {
-    return { number, doctor, patient, date, time };
-  }
-  const rows = [
-    createData("001", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
-    createData(
-      "002",
-      "Махмудходжаева. Д",
-      "Мансурова А.П",
-      "13.05.2022",
-      "09:50"
-    ),
-    createData("003", "Паненко. Н", "Мансурова А.П", "13.05.2022", "09:50"),
-    createData("004", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
-    createData(
-      "005",
-      "Махмудходжаева. Д",
-      "Мансурова А.П",
-      "13.05.2022",
-      "09:50"
-    ),
-    createData("006", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
-  ];
-  return (
-    <TableContainer>
-      <Table>
-        <TableHead width={"100%"}>
-          {/* <TableRow>
+	const columns = [
+		{ id: "number", label: "№", width: 35 },
+		{ id: "doctor", label: "ФИО врача", minWidth: 150 },
+		{
+			id: "patient",
+			label: "ФИО пациента",
+			minWidth: 150,
+			format: (value) => value.toLocaleString("en-US"),
+		},
+		{
+			id: "date",
+			label: "Дата",
+			minWidth: 100,
+			format: (value) => value.toLocaleString("en-US"),
+		},
+		{
+			id: "time",
+			label: "Время",
+			width: 100,
+			format: (value) => value.toFixed(2),
+		},
+		{
+			id: 'option',
+			label: '',
+			width: 20,
+		}
+	];
+	function createData(number, doctor, patient, date, time) {
+		return { number, doctor, patient, date, time };
+	}
+	const rows = [
+		createData("001", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
+		createData(
+			"002",
+			"Махмудходжаева. Д",
+			"Мансурова А.П",
+			"13.05.2022",
+			"09:50"
+		),
+		createData("003", "Паненко. Н", "Мансурова А.П", "13.05.2022", "09:50"),
+		createData("004", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
+		createData(
+			"005",
+			"Махмудходжаева. Д",
+			"Мансурова А.П",
+			"13.05.2022",
+			"09:50",
+		),
+		createData("006", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
+		createData("001", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
+		createData(
+			"002",
+			"Махмудходжаева. Д",
+			"Мансурова А.П",
+			"13.05.2022",
+			"09:50"
+		),
+		createData("003", "Паненко. Н", "Мансурова А.П", "13.05.2022", "09:50"),
+		createData("004", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
+		createData(
+			"005",
+			"Махмудходжаева. Д",
+			"Мансурова А.П",
+			"13.05.2022",
+			"09:50",
+		),
+		createData("006", "Сабиров Ш.И", "Мансурова А.П", "13.05.2022", "09:50"),
+	];
+	const calcHeight = (px) => {
+		const windowHeight = window.innerHeight;
+		const height = windowHeight - px;
+		console.log(height)
+		return height
+	}
+	return (
+		<TableContainer
+			className={styles.tableContainer}
+			sx={{
+				height: calcHeight(360),
+			}}
+		>
+			<Table
+				stickyHeader aria-label="sticky table"
+				sx={{
+					height: calcHeight(500),
+					paddingRight: '24px'
+				}}
+			>
+				<TableHead width={"100%"}
+
+				>
+					{/* <TableRow>
             <TableCell>№</TableCell>
             <TableCell>ФИО врача</TableCell>
             <TableCell>ФИО пациента</TableCell>
             <TableCell>Дата</TableCell>
             <TableCell>Время</TableCell>
 					</TableRow> */}
-          <TableRow width={"100%"}>
-            {columns.map((column) => (
-              <TableCell
-                key={column.id}
-                // align={column.align}
-                style={{
-                  // top: 57,
-                  padding: "15px 10px",
-                  width: column.width,
-                  minWidth: column.minWidth,
-                  maxWidth: column.maxWidth,
-                }}
-              >
-                {column.label}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => {
-            return (
-              <TableRow key={row.number}>
-                {columns.map((column) => {
-                  const value = row[column.id];
-                  return (
-                    <TableCell
-                      key={column.id}
-                      style={{
-                        // top: 57,
-                        padding: "15px 10px",
-                        width: column.width,
-                        minWidth: column.minWidth,
-                        maxWidth: column.maxWidth,
-                      }}
-                    >
-                      {value}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+					<TableRow width={"100%"}>
+						{columns.map((column) => (
+							<TableCell
+								key={column.id}
+								// align={column.align}
+								style={{
+									// top: 57,
+									padding: "15px 10px",
+									width: column.width,
+									minWidth: column.minWidth,
+									maxWidth: column.maxWidth,
+									borderBottom: '1px solid #4C464B'
+								}}
+							>
+								{column.label}
+							</TableCell>
+						))}
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => {
+						return (
+							<TableRow key={row.number}>
+								{columns.map((column) => {
+									const value = row[column.id];
+									return (
+										<TableCell
+											key={column.id}
+											style={{
+												// top: 57,
+												padding: "15px 10px",
+												width: column.width,
+												minWidth: column.minWidth,
+												maxWidth: column.maxWidth,
+												borderBottom: 'none'
+											}}
+										>
+											{value}
+											{column.id === 'option' ? <button className={styles.tableBtn} onClick={()=>alert('clicked option button')}><img src={icons.dots} alt={column.id} /></button> : null}
+										</TableCell>
+									);
+								})}
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 };
