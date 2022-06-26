82dd241f371765efcaf3b075a9efcc27f43dfd44
mainazarov1 <mainazarov1@gmail.com>
2022-06-26 20:07:06 +0600

    T3-232 - feat: modifie icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,11 +4,13 @@ import calendar from './calendar.svg'
 import checkList from './checkList.svg'
 import medTechWhite from './logo_med_tech-white.svg'
 import arrow from './arrow.svg'
+import dots from './dotsThreeVertical.svg'
 export const icons = {
 	medTech,
 	medTechWhite,
 	medTechViolet,
 	calendar,
 	checkList,
-	arrow
+	arrow,
+	dots
 }
