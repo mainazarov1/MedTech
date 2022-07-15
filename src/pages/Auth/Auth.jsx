18975f220e4306dbaf67655aadf988327d21e954
mainazarov1 <mainazarov1@gmail.com>
2022-07-15 20:54:02 +0600

    T3-285 - fix: incorrect titles

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,50 +1,59 @@
-import { Box, Grid } from "@mui/material";
+import { Box, CircularProgress, Grid } from "@mui/material";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import Progress from "../../components/Progress/Progress";
 import {
-  FormSignIn,
-  FormRecoveryWithLogin,
-  FormRecoveryWithPass,
-  FormRecoveryWithNewPass,
+	FormSignIn,
+	FormRecoveryWithLogin,
+	FormRecoveryWithPass,
+	FormRecoveryWithNewPass,
 } from "./../../components/Forms";
-import style from "./Auth.module.css";
+import styles from "./Auth.module.css";
+import { ToastContainer, toast } from 'react-toastify';
+import { Toast } from "../../components/Toast/Toast";
 
 const Auth = () => {
-  const [step, setStep] = useState(1);
+	const [step, setStep] = useState(1);
+	const {
+		isLoading,
+		isSuccess,
+		message,
+		user } = useSelector((state) => state.auth)
+	const nextStep = () => {
+		setStep(step + 1);
+	};
+	const prevStep = () => {
+		setStep(step - 1);
+	};
 
-  const nextStep = () => {
-    setStep(step + 1);
-  };
-  const prevStep = () => {
-    setStep(step - 1);
-  };
+	const view = () => {
+		switch (step) {
+			case 1:
+				return <FormSignIn nextStep={nextStep} prevStep={prevStep} />;
+			case 2:
+				return <FormRecoveryWithLogin nextStep={nextStep} prevStep={prevStep} />;
+			case 3:
+				return <FormRecoveryWithPass nextStep={nextStep} prevStep={prevStep} />;
+			case 4:
+				return <FormRecoveryWithNewPass nextStep={nextStep} prevStep={prevStep} />;
+			default:
+				return;
+		}
+	};
 
-  const view = () => {
-    switch (step) {
-      case 1:
-        return <FormSignIn nextStep={nextStep} prevStep={prevStep} />;
-      case 2:
-        return (
-          <FormRecoveryWithLogin nextStep={nextStep} prevStep={prevStep} />
-        );
-      case 3:
-        return <FormRecoveryWithPass nextStep={nextStep} prevStep={prevStep} />;
-      case 4:
-        return (
-          <FormRecoveryWithNewPass nextStep={nextStep} prevStep={prevStep} />
-        );
-      default:
-        return;
-    }
-  };
-  return (
-    <Grid container height="100vh">
-      <Grid item xs={0} sm={6}>
-        <div className={style.auth__bg}></div>
-      </Grid>
-      <Grid item xs={12} sm={6} display="flex">
+
+	return (
+		<Grid className={styles.auth} container height="100vh">
+			{isLoading ?
+				<Progress />
+				: null}
+			<Grid className={styles.auth__left} item xs={0} sm={6}>
+				<div className={styles.auth__bg}></div>
+			</Grid>
+			<Grid className={styles.auth__right} item xs={12} sm={6} display="flex">
 				{view()}
-      </Grid>
-    </Grid>
-  );
+			</Grid>
+		</Grid>
+	);
 };
 export default Auth;
