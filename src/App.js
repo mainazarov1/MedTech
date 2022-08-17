e959e83ccec589285536aca3d5a5be847d3e7fcf
mainazarov1 <mainazarov1@gmail.com>
2022-08-17 23:12:16 +0600

    T3-285 - feat: change styles and dependencies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-// React Router
-import { Route, Routes } from "react-router-dom";
+// React, Redux, Router
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes, useNavigate } from "react-router-dom";
+// Toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // Components
+import { Progress } from "./components/Progress/Progress";
 import Auth from "./pages/Auth/Auth";
-import { DocWeb } from "./pages/DocWeb/DocWeb";
-import { Main } from "./pages/Main/Main";
+import Main from "./pages/Main/Main";
+import DocWeb from "./pages/DocWeb/DocWeb";
 
 function App() {
+	const { isLoading, isLogged } = useSelector((state) => state.auth)
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!isLogged) navigate('/');
+	}, [isLogged, navigate]);
 	return (
 		<div className="App">
+			{isLoading ?
+				<Progress />
+				: null}
+			<ToastContainer
+				style={{
+					width: 'fit-content',
+				}}
+				autoClose={2000}
+			/>
 			<Routes>
-				<Route path="/manual" element={<DocWeb />} />
 				<Route path='/' element={<Auth />} />
-				<Route path='/*' element={<Main />} />
+				{isLogged &&
+					<>
+						<Route path="/manual" element={<DocWeb />} />
+						<Route path='/*' element={<Main />} />
+					</>}
 			</Routes>
 		</div>
 	);
